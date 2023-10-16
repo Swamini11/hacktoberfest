d1afee42dcd78b7f7b3ad9a1bb197ab352a6451f
Swamini11 <97759943+Swamini11@users.noreply.github.com>
2023-10-16 22:18:14 +0530

    Create 3Sum_leetcode.java

---
+++ Java/3Sum_leetcode.java
@@ -0,0 +1,53 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int left = i + 1;
+                int right = nums.length - 1;
+                int target = 0 - nums[i];
+
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        List<Integer> triplets = new ArrayList<>();
+                        triplets.add(nums[i]);
+                        triplets.add(nums[left]);
+                        triplets.add(nums[right]);
+                        ans.add(triplets);
+
+                        // Skip duplicate elements
+                        while (left < nums.length - 1 && nums[left] == nums[left + 1]) left++;
+                        while (right > 0 && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
